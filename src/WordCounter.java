f9e67b0532dee0a24afa6e0d69244575450f2fd8
rohitbams <143954072+rohitbams@users.noreply.github.com>
2024-10-09 17:38:58 +0530

    Update WordCounter.java
    did stuff on mbp

--- src/WordCounter.java
+++ src/WordCounter.java
@@ -16,16 +16,19 @@ public class WordCounter {
                 return;
             }
 
+
             String fileName = args[0];
             String[] words = new String[args.length - 1];
             String line = "";
             ArrayList<String> lines = new ArrayList<>();
             int totalCount = 0;
+            System.out.println(Arrays.toString(words));
 
             File file = new File(fileName);
             if (!file.exists()) {
                 System.out.println("File not found: " + fileName);
             }
+
             BufferedReader br = new BufferedReader(new FileReader(fileName));
             while ((line = br.readLine()) != null) {
                 lines.add(line);
@@ -34,46 +37,50 @@ public class WordCounter {
 
             for (int i = 1; i < args.length; i++) {
                 words[i - 1] = args[i];
-                totalCount = count;
+                // System.out.println("Assigned word: " + words[i - 1]);
+
                 count = 0;// reset the counter
+
+                /* this for-each loop
+                 *
+                 */
                 for (String lineFromList : lines) {
                     Pattern patter = Pattern.compile("\\b" + words[i - 1] + "\\b");
                     Matcher matcher = patter.matcher(lineFromList);
 
+                    /* this while loop matches the word
+                     * and counts its appearances in each line of the text
+                     */
+
                     while (matcher.find()) {
                         count++;
                     }
-                }
-                //List<String> strings = Arrays.asList(words);
-                //String longest = strings.stream().max(Comparator.comparingInt(String::length)).get();
-                //int max = longest.length();
 
-//                if (words.length < 3) {
+                }
+                totalCount += count;
+//                List<String> strings = Arrays.asList(words);
+//                String longest = strings.stream().max(Comparator.comparingInt(String::length)).get();
+//                int max = longest.length();
+
+                //if (words.length < 3) {
+
+
+                if (args.length < 3) {
                     if (count == 0) {
                         System.out.println("The word '" + words[i - 1] + "' appears " + count + " times.");
                     } else if (count == 1) {
                         System.out.println("The word '" + words[i - 1] + "' appears " + +count + " time.");
                     } else {
-                        System.out.println("The word '" + words[i - 1] + "' appears " + +count + " times.");
-                        //Arrays.stream(words).forEach(System.out::println);
-                    //}
-//                } else {
-//                    String nline = "\n";
-
-                    //System.out.println(countArray);
-//                    String.join(nline, words);
-//                    System.out.printf("|---------------------------------|%n");
-//                    System.out.printf("|     Word      |      Count      |%n");
-//                    System.out.printf("|---------------------------------|%n");
-//                    Arrays.stream(words).forEach(System.out::println);
-//                    System.out.println(words);
-//                    System.out.println("total " + totalCount);
-
-
-
+                        System.out.println("The word '" + words[i - 1] + "' appears " + +count + " times." + totalCount);
                     }
+                } else {
+                    System.out.printf("| %-15s | %-10d |%s", "Word", "Count");
+                    System.out.printf("| %-15s | %-10d |%n", words[i - 1], count);
+
+
                 }
-            } catch (IOException e) {
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
