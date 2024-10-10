50b04456447e47410e93dd1b89a7276b8ed910cb
rohitbams <143954072+rohitbams@users.noreply.github.com>
2024-10-10 15:55:10 +0530

    Update WordCounter.java
    add colour to table

--- src/WordCounter.java
+++ src/WordCounter.java
@@ -1,4 +1,3 @@
-import java.io.File;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -22,11 +21,10 @@ public class WordCounter {
             ArrayList<String> lines = new ArrayList<>();
             int totalCount = 0;
 
-            File file = new File(fileName);
-            if (!file.exists()) {
-                System.out.println("File not found: " + fileName);
-                return;
-            }
+            String RESET = "\u001B[0m";
+            String RED_TEXT = "\u001B[31m";
+            String BLUE_TEXT = "\u001B[32m";
+            String YELLOW_TEXT ="\u001B[33m";
 
             BufferedReader br = new BufferedReader(new FileReader(fileName));
             while ((line = br.readLine()) != null) {
@@ -40,19 +38,23 @@ public class WordCounter {
                 words[i - 1] = args[i];
             }
 
+            /**
+             *  this for loop fills the words[] array with the values from
+             *  the args[] array from index args[1] onwards
+             *  and finds the longest string in the words[] array
+             *  and assigns it's to the int variable longestWordLength
+             */
             for (int i = 0; i < words.length; i++) {
                 String word = words[i];
                 longestWordLength = Math.max(longestWordLength, word.length());
 
-                count[i] = 0; // reset the counter
-
-                /* this for-each loop
+                /** this for-each loop
                  *
                  */
                 for (String lineFromList : lines) {
-                    Pattern pattern = Pattern.compile("\\b" + word + "\\b");
+                    Pattern pattern = Pattern.compile("\\b" + word + "\\b", Pattern.CASE_INSENSITIVE);
                     Matcher matcher = pattern.matcher(lineFromList);
-                    /* this while loop matches the word
+                    /** this while loop matches the word
                      * and counts its appearances in each line of the text
                      */
                     while (matcher.find()) {
@@ -70,18 +72,19 @@ public class WordCounter {
                     System.out.println("The word '" + words[0] + "' appears " + count[0] + " times.");
                 }
             } else {
-                System.out.println("|" + "-".repeat(longestWordLength < 6 ? 7 : longestWordLength + 2) + "|" + "-".repeat(7) + "|");
+                String totalCountString = Integer.toString(totalCount);
+                System.out.println(RED_TEXT + "|" + "-".repeat(longestWordLength < 6 ? 7 : longestWordLength + 2) + "|" + "-".repeat(7) + "|");
                 System.out.printf("| %-" + (longestWordLength < 4 ? 5 : longestWordLength) + "s | %s %s %n", "WORD", "COUNT", "|");
-                System.out.println("|" + "-".repeat(longestWordLength < 6 ? 7 : longestWordLength + 2) + "|" + "-".repeat(7) + "|");
+                System.out.println("|" + "-".repeat(longestWordLength < 6 ? 7 : longestWordLength + 2) + "|" + "-".repeat(7) + "|" + RESET);
                 for (int i = 0; i < words.length; i++) {
-                    System.out.printf("| %-" + (longestWordLength < 4 ? 5 : longestWordLength) + "s | %" + (5) + "s %s %n", words[i], count[i], "|");
+                    System.out.printf(BLUE_TEXT + "| %-" + (longestWordLength < 4 ? 5 : longestWordLength) + "s | %" + (5) + "s %s %n", words[i], count[i], "|" + RESET);
                 }
-                System.out.println("|" + "-".repeat(longestWordLength < 6 ? 7 : longestWordLength + 2) + "|" + "-".repeat(7) + "|");
-                System.out.printf("| %-" + (longestWordLength < 5 ? 5 : longestWordLength) + "s | %" + (5) + "s %s %n", "Total", totalCount, "|");
-                System.out.println("|" + "-".repeat(longestWordLength < 6 ? 7 : longestWordLength + 2) + "|" + "-".repeat(7) + "|");
+                System.out.println(YELLOW_TEXT + "|" + "-".repeat(longestWordLength < 6 ? 7 : longestWordLength + 2) + "|" + "-".repeat(7) + "|");
+                System.out.printf("| %-" + (longestWordLength < 5 ? 5 : longestWordLength) + "s | %" + (5) + "s %s %n", "Total", totalCountString, "|");
+                System.out.println("|" + "-".repeat(longestWordLength < 6 ? 7 : longestWordLength + 2) + "|" + "-".repeat(totalCountString.length() > 5 ? totalCountString.length() + 2 : 7) + "|" + RESET);
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println("File not found: " + args[0]);
         }
     }
 }
